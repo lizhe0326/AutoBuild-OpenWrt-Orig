6ffc9c9c11ffbfb077cb8304d6b31df35c7ca15b
Cosmos-Break <438854233@qq.com>
2023-12-21 15:31:31 +0800

    push

--- complie_command.sh
+++ complie_command.sh
@@ -2,7 +2,8 @@ git clone git@github.com:openwrt/openwrt.git
 cd openwrt
 git fetch -t
 # git tag -l
-latest_tag=`git tag --sort=committerdate | tail -1`
+# latest_tag=`git tag --sort=committerdate | tail -1`
+latest_tag=v23.05.2
 git checkout ${latest_tag}
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
@@ -14,6 +15,10 @@ cp ../x86_64.config .config
 make defconfig
 make menuconfig
 # ./scripts/diffconfig.sh > ../diffconfig
+
+# cp ../K2P.config .config
+# cp ../CR6608.config .config
+# cp ../AX6S.config .config
 
 
 # 二次编译：
