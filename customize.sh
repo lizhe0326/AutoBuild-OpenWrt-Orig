cbeac8531ee8ccff28480ce9c3bf2749af14420b
Cosmos-Break <438854233@qq.com>
2024-03-24 13:54:11 +0800

    push

--- customize.sh
+++ customize.sh
@@ -13,3 +13,10 @@ sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config
 
 #3. Replace with JerryKuKu’s Argon
 #rm openwrt/package/lean/luci-theme-argon -rf
+
+
+
+base-files busybox ca-bundle dnsmasq dropbear firewall4 fitblk fstools kmod-crypto-hw-safexcel kmod-gpio-button-hotplug kmod-leds-gpio kmod-mt7915e kmod-mt7981-firmware kmod-nft-offload kmod-phy-aquantia libc libgcc libustream-mbedtls logd luci mt7981-wo-firmware mtd netifd nftables odhcp6c odhcpd-ipv6only opkg ppp ppp-mod-pppoe procd procd-seccomp procd-ujail uboot-envtools uci uclient-fetch urandom-seed urngd wpad-openssl luci-app-dawn luci-app-ttyd
+
+
+base-files busybox ca-bundle dnsmasq dropbear firewall4 fitblk fstools kmod-crypto-hw-safexcel kmod-gpio-button-hotplug kmod-leds-gpio kmod-mt7915e kmod-mt7981-firmware kmod-nft-offload kmod-phy-aquantia libc libgcc libustream-mbedtls logd luci mt7981-wo-firmware mtd netifd nftables odhcp6c odhcpd-ipv6only opkg ppp ppp-mod-pppoe procd procd-seccomp procd-ujail uboot-envtools uci uclient-fetch urandom-seed urngd cgi-io dawn iperf3 libatomic libcap libgcrypt libgpg-error libiperf3 liblua liblucihttp liblucihttp-lua liblucihttp-ucode libopenssl libopenssl-conf libopenssl-legacy librt libubus-lua libuv libwebsockets-full lua luci luci-app-dawn luci-app-firewall luci-app-opkg luci-app-ttyd luci-base luci-compat luci-i18n-base-zh-cn luci-i18n-firewall-zh-cn luci-i18n-opkg-zh-cn luci-i18n-ttyd-zh-cn luci-lib-base luci-lib-ip luci-lib-jsonc luci-lib-nixio luci-light luci-lua-runtime luci-mod-admin-full luci-mod-network luci-mod-status luci-mod-system luci-proto-ipv6 luci-proto-ppp luci-theme-bootstrap rpcd rpcd-mod-file rpcd-mod-iwinfo rpcd-mod-luci rpcd-mod-rrdns rpcd-mod-ucode ttyd ucode-mod-html ucode-mod-lua ucode-mod-math uhttpd uhttpd-mod-ubus umdns wpad-openssl zlib
